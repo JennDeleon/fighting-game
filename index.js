c5fcbe596dadb1ae3b24902412a382b98eeb1dc3
Jennifer Deleon <jennifer.deleon404@gmail.com>
2022-07-28 18:34:35 -0500

    adding files

---
+++ index.js
@@ -0,0 +1,10 @@
+//For sizing the screen
+const canvas = document.querySelector('canvas');
+
+// Selecting canvas context(c), for 2D game
+const c = canvas.getContext('2d');
+
+//Sizing Canvas
+canvas.width = 1024
+canvas.width = 576
+
