54349012ab63b79c00d2175918226fc9ed0decbd
Jennifer Deleon <jennifer.deleon404@gmail.com>
2022-07-28 19:18:36 -0500

    corrected small location of script error

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
 
 //Sizing Canvas
-canvas.width = 1024
-canvas.width = 576
+canvas.width = 1024;
+canvas.height = 576;
+
+//Canvas background
+c.fillRect(0, 0, 1024, 576);
 
