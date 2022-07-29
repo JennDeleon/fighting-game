1ce9451bc2fb8eb1b17dbcd309b491cd6648673d
Jennifer Deleon <jennifer.deleon404@gmail.com>
2022-07-28 19:27:10 -0500

    added first players

--- index.js
+++ index.js
@@ -11,3 +11,29 @@ canvas.height = 576;
 //Canvas background
 c.fillRect(0, 0, 1024, 576);
 
+//Blueprint for the object before we create it
+class Sprite {
+    constructor(position) {
+        this.position = position
+    }
+    //drawing sprite
+    draw() {
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, 50, 150)
+    }
+}
+
+//creating player and starting position
+const player = new Sprite({
+    x: 0,
+    y: 0
+})
+
+player.draw()
+
+const enemy = new Sprite({
+    x: 400,
+    y: 100
+})
+
+enemy.draw()
